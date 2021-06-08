b3199c59fca9f5f3ff880af44b4cc2fa646ccf01
Sergey <hlypik0@gmail.com>
2021-06-08 19:43:06 -0400

    Done

--- main_second_option.cpp
+++ main_second_option.cpp
@@ -10,5 +10,45 @@ int main ()
   {
     cout << i + 1 << "." << days[i] << endl;
   }
+  int numbDay = 0;
+  cout << "Enter number of day: "; cin >> numbDay;
+  switch (numbDay)
+  {
+    case 1:
+    {
+    cout << days[0] << endl;
+    break;
+    }
+    case 2:
+    {
+    cout << days[1] << endl;
+    break;
+    }
+    case 3:
+    {
+    cout << days[2] << endl;
+    break;
+    }
+    case 4:
+    {
+    cout << days[3] << endl;
+    break;
+    }
+    case 5:
+    {
+    cout << days[4] << endl;
+    break;
+    }
+    case 6:
+    {
+    cout << days[5] << endl;
+    break;
+    }
+    case 7:
+    {
+    cout << days[6] << endl;
+    break;
+    }
+  }
   return 0;
 }
